67748e44ddf3761ee88174f56cdb89db5c7dac3f
subham saha <subhamsaha132003@gmail.com>
2025-01-06 22:12:13 +0530

    3sum

---
+++ 3sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+        vector<vector<int>> threeSum(vector<int>& nums) {
+                int target = 0;
+                        sort(nums.begin(), nums.end());
+                                set<vector<int>> s;
+                                        vector<vector<int>> output;
+                                                for (int i = 0; i < nums.size(); i++){
+                                                            int j = i + 1;
+                                           …                int k = nums.size() - 1;
+…                                                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                …                                                                                    return output;
+                                                                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                      }
+                                                                                                                                                                                                                                                                                                                            };
